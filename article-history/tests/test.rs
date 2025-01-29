7f0db122ed2736bfcd40e676e586ecff28c95e0c
Helena de Oliveira <helenadeoliveira1@proton.me>
2025-01-29 08:38:23 -0300

    Removed unused tasks

--- article-history/tests/test.rs
+++
@@ -1,8 +0,0 @@
-use article_history_rs::TemplateNameMap;
-
-fn build_static_template_type_map() -> TemplateNameMap {
-    TemplateType::iter()
-        .map(|t| (&t.get_template_title()[9..], Either::Right(t)))
-        .chain(("Article history", Either::Left(ArticleHistoryTemplateType)))
-        .collect()
-}
